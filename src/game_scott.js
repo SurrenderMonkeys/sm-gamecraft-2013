a9268a07d796e5802139e99061e17c433e9d8ac4
Scott Mason <scott@madebymade.co.uk>
2013-08-08 22:07:45 +0100

    add assets and index for my own dig around in crafty, plus a newer version of crafty since 0.4.9 was causing problems

---
+++ src/game_scott.js
@@ -0,0 +1,8 @@
+Game = {
+  // Initialize and start the game
+  start: function() {
+    // start crafty and set a backround color so that we can it's working
+    Crafty.init(480, 320);
+    Crafty.background('green');
+  }
+}
